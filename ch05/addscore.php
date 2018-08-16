b339036e6cc07b51c32f0925d87d7994931d45a2
Li Chen <chen1649chenli@gmail.com>
2018-08-16 08:30:54 -0500

    added file type verification

--- ch05/addscore.php
+++ ch05/addscore.php
@@ -17,30 +17,38 @@
     // Grab the score data from the POST
     $name = $_POST['name'];
     $score = $_POST['score'];
+    $screenshot_type = $_FILES['screenshot']['type'];
+    $screenshot_size = $_FILES['screenshot']['size'];
     $screenshot = $_FILES['screenshot']['name'];
     if (!empty($name) && !empty($score) && !empty($screenshot)) {
-      $target = GW_UPLOADPATH.$screenshot;
-      if(move_uploaded_file($_FILES['screenshot']['tmp_name'],$target)){
-        // Connect to the database
-        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-          or die("Can't connect to the database!");
-        // Write the data to the database
-        $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score','$screenshot')";
-        mysqli_query($dbc, $query)
-          or die("Can't insert the records into the table");
-        // Confirm success with the user
-        echo '<p>Thanks for adding your new high score!</p>';
-        echo '<p><strong>Name:</strong> ' . $name . '<br />';
-        echo '<strong>Score:</strong> ' . $score . '</p>';
-        echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
-        // Clear the score data to clear the form
-        $name = "";
-        $score = "";
-        $screenshot = "";
-        mysqli_close($dbc);
+      if ((($screenshot_type == 'image/gif') ||($screenshot_type == 'image/jpeg')||($screenshot_type == 'image/pjpeg')||($screenshot_type == 'image/png')) && (($screenshot_size >0)&&($screenshot_size < GW_MAXFILESIZE))){
+        $target = GW_UPLOADPATH.$screenshot;
+        if(move_uploaded_file($_FILES['screenshot']['tmp_name'],$target)){
+          // Connect to the database
+          $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+            or die("Can't connect to the database!");
+          // Write the data to the database
+          $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score','$screenshot')";
+          mysqli_query($dbc, $query)
+            or die("Can't insert the records into the table");
+          // Confirm success with the user
+          echo '<p>Thanks for adding your new high score!</p>';
+          echo '<p><strong>Name:</strong> ' . $name . '<br />';
+          echo '<strong>Score:</strong> ' . $score . '</p>';
+          echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
+          // Clear the score data to clear the form
+          $name = "";
+          $score = "";
+          $screenshot = "";
+          mysqli_close($dbc);
+        }else{
+          echo '<p class="error">There is a problem uploading your image.</p>';
+        }
       }else{
-        '<p class="error">There is a problem uploading your image.</p>';
+        echo '<p class="error">The screen shot must be a GIF, JPEG, or PNG image file no greater than ' . (GW_MAXFILESIZE / 1024) . ' KB in size.</p>';
+        @unlink($_FILES['screenshot']['tmp_name']);
       }
+
     }else {
       echo '<p class="error">Please enter all of the information to add your high score.</p>';
     }
