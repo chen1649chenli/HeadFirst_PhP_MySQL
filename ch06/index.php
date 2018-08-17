87b4b59558e2f30b326680fd45e52f926ecf7318
Li Chen <chen1649chenli@gmail.com>
2018-08-16 20:50:58 -0500

    added the page protection

--- ch06/index.php
+++ ch06/index.php
@@ -12,22 +12,33 @@
   <hr />
 
 <?php
-  //require_once('appvars.php');
-  //require_once('connectvars.php');
+  require_once('appvars.php');
+  require_once('connectvars.php');
   // Connect to the database
-  //$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-  $dbc = mysqli_connect('localhost', 'root', 'chen001649', 'guitar_war')
-    or die('Error connecting to MySQL server.');
+  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
   // Retrieve the score data from MySQL
-  $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC"
-    or die('Error querying the database.');
+  $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
   $data = mysqli_query($dbc, $query);
   // Loop through the array of score data, formatting it as HTML
+  echo '<table>';
   $i = 0;
   while ($row = mysqli_fetch_array($data)) {
     // Display the score data
-    echo $row['score']."\t".$row['name']."\t".$row['date']."\n\n";
+    if ($i==0){
+      echo '<tr><td colspan="2" class="topscoreheader" > TopScore:'.$row['score'].'</td><tr>';
+    }
+    echo '<tr><td class="scoreinfo">';
+    echo '<span class="score">' . $row['score'] . '</span><br />';
+    echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
+    echo '<strong>Date:</strong> ' . $row['date'] . '</td></tr>';
+    if (is_file(GW_UPLOADPATH.$row['screen_shot']) && filesize(GW_UPLOADPATH.$row['screen_shot'])>0){
+      echo '<td><img src="'.GW_UPLOADPATH.$row['screen_shot'].'"alt="Score image" /></td> </tr>';
+    }else{
+      echo '<td><img src="'.GW_UPLOADPATH. 'unverified.gif'.'"alt="Unverified score" /></td> </tr>';
+    }
+    $i++;
   }
+  echo '</table>';
   mysqli_close($dbc);
 ?>
 
